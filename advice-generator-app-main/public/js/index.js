ceee9584685b51b39f843d94695237c836176931
camilafbc <camilafernanda914@gmail.com>
2023-05-07 18:36:52 -0300

    finalizando projeto

--- advice-generator-app-main/public/js/index.js
+++ advice-generator-app-main/public/js/index.js
@@ -1,11 +1,11 @@
 const container = document.querySelector(".container");
-async function getAdvice() {
+const idEntry = document.getElementById("id-entry");
+const adviceEntry = document.getElementById("advice-entry");
+async function fetchAdvice() {
     const response = await fetch('https://api.adviceslip.com/advice');
     const advice = await response.json();
-    container.innerHTML = `
-       <p>
-            Advice #${advice.slip.id} \n "${advice.slip.advice}"
-       </p> 
-    `;
+    idEntry.innerText = `ADVICE #${advice.slip.id}`;
+    adviceEntry.innerText = `"${advice.slip.advice}"`;
 }
-getAdvice();
+fetchAdvice();
+document.querySelector(".btn-container").addEventListener('click', fetchAdvice);
---
+++ advice-generator-app-main/src/index.ts
@@ -0,0 +1,25 @@
+const container: HTMLElement = document.querySelector(".container")
+const idEntry = document.getElementById("id-entry")
+const adviceEntry = document.getElementById("advice-entry")
+
+interface Slip{
+    slip: adviceResponse
+}
+
+interface adviceResponse {
+    id: number
+    advice: string
+}
+
+async function fetchAdvice(){
+    const response = await fetch('https://api.adviceslip.com/advice')
+    const advice: Slip = await response.json()
+
+    idEntry.innerText = `ADVICE #${advice.slip.id}`
+    adviceEntry.innerText = `"${advice.slip.advice}"`
+}
+
+fetchAdvice()
+
+document.querySelector(".btn-container").addEventListener('click', fetchAdvice)
+
